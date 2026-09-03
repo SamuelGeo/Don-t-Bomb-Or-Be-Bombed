2f8cab21075ff7f041a3a229921d0e49181e6680
SamuelGeo <166186924+SamuelGeo@users.noreply.github.com>
2026-09-03 00:45:00 -0400

    Added Shader Loading Method Calls and Global Properties

--- Mods/BaseMod/HookFunctions.py
+++ Mods/BaseMod/HookFunctions.py
@@ -8,49 +8,49 @@ from Mods.BaseMod.Sprites.Images import *
 from Mods.BaseMod.HookScreenObject import *
 from Mods.BaseMod.HelperFunctions import *
 from Mods.BaseMod.Modules import Module
-
+from Mods.BaseMod.HookVariables import GlobalProperties as GP
+from Mods.BaseMod.BackroundShaders import CalculateShader
+from Mods.BaseMod.BackroundShaders import SetBackroundShader
+from Mods.BaseMod.BackroundShaders import NumberOfShaders
+from Mods.BaseMod.BackroundShaders import DifferencyScore
+from Mods.BaseMod.BackroundShaders import ShaderFPS
+from Mods.BaseMod.BackroundShaders import ShadersResolution
+from Mods.BaseMod.BackroundShaders import SelectedShader
+from Mods.BaseMod.BackroundShaders import LoadShader
 import random
 
-MouseCoords = [0,0]
-MousePressed = False
-ButtonPresses = [False, False, False]
-DistanceMesurement = 10000
-Tilted = False
-KeyPadInputs = [False, False, False, False, False, False, False, False]
-GameWindowWidthHeight = (0,0)
-CurrentTime = 0.0
+def SetBackroundSurfaceSize(SurfaceSizeIn) :
 
-GameDifficulty = 0
+    SurfaceScaleBy = max(SurfaceSizeIn[0]/ShadersResolution, SurfaceSizeIn[1]/ShadersResolution)
+   
+    return (int(SurfaceSizeIn[0]/SurfaceScaleBy),int(SurfaceSizeIn[0]/SurfaceScaleBy))
 
-#0: The game is playing
-#1: The game is won
-#2: The game is lost
-GameState = 0
+def LoadThisShader(ShaderIndex, SurfaceSize) :
+
+    return LoadShader(ShaderIndex, SurfaceSize)
 
 def InitializeObjects(GameMode, GameWindowSize) :
+    
+    GP.GameWindowWidthHeight = GameWindowSize
 
-    global GameWindowWidthHeight
-    GameWindowWidthHeight = GameWindowSize
-
-    global GameDifficulty
-    GameDifficulty = GameMode
+    GP.GameDifficulty = GameMode
 
     ModulesList = Module.AvailableModules.copy()
     
     ModuleSize = GetImageWidthHeight(GetImage(ModulesList[0]))
-    SizeOfModule = GetMaxPoint(-1, -1, ModuleSize[0], ModuleSize[1], GameWindowWidthHeight[0], GameWindowWidthHeight[1])
+    SizeOfModule = GetMaxPoint(-1, -1, ModuleSize[0], ModuleSize[1], GP.GameWindowWidthHeight[0], GP.GameWindowWidthHeight[1])
     print(ModuleSize[0])
 
     SizeOfModule = (SizeOfModule[0], SizeOfModule[1])
     #quit()
     AvailableSlots = []
   
-    for ThisSlotY in range(0,GameWindowWidthHeight[1], ModuleSize[1]) :
+    for ThisSlotY in range(0,GP.GameWindowWidthHeight[1], ModuleSize[1]) :
         
-        for ThisSlotX in range(0,GameWindowWidthHeight[0], ModuleSize[0]) :
+        for ThisSlotX in range(0,GP.GameWindowWidthHeight[0], ModuleSize[0]) :
 
-            if ThisSlotY + ModuleSize[1] <= GameWindowWidthHeight[1] and ThisSlotX + ModuleSize[0] <= GameWindowWidthHeight[0]:
-                AvailableSlots.append(GetNormalizedCoordinates(int(ThisSlotX), int(ThisSlotY), GameWindowWidthHeight[0], GameWindowWidthHeight[1]))
+            if ThisSlotY + ModuleSize[1] <= GP.GameWindowWidthHeight[1] and ThisSlotX + ModuleSize[0] <= GP.GameWindowWidthHeight[0]:
+                AvailableSlots.append(GetNormalizedCoordinates(int(ThisSlotX), int(ThisSlotY), GP.GameWindowWidthHeight[0], GP.GameWindowWidthHeight[1]))
 
     SlotsPositions = AvailableSlots
 
@@ -58,7 +58,7 @@ def InitializeObjects(GameMode, GameWindowSize) :
     #SlotsPositions = [(-1,-1) , (-0.325, -1), (0.34, -1), (-1, -0.01), (-0.325, -0.01), (0.34, -0.01)]
 
     #The easiest difficulty, with only two modules to solve 
-    if GameDifficulty == 0 :
+    if GP.GameDifficulty == 0 :
 
         ModulesList = Module.AvailableModules.copy()
 
@@ -77,7 +77,7 @@ def InitializeObjects(GameMode, GameWindowSize) :
 
             ModuleAdded = Module(ModulesList[0], SlotsPositions[ModuleIterator][0], SlotsPositions[ModuleIterator][1])
 
-    elif GameDifficulty == 1 :
+    elif GP.GameDifficulty == 1 :
         ModulesList = Module.AvailableModules.copy()
 
         ModuleIterator = 0
@@ -95,27 +95,27 @@ def InitializeObjects(GameMode, GameWindowSize) :
 
             ModuleAdded = Module(ModulesList[0], SlotsPositions[ModuleIterator][0], SlotsPositions[ModuleIterator][1])
 
-
-
-def SetBombTime(SetValue) :
-
-    global CurrentTime
-    global BombTime
-    BombTime = SetValue
-    print(str(CurrentTime) + " " + str(BombTime))
-
-
-
-
 def LoadImagesToEngine() :
     #Loads all of the images to the engine
     return GetImagesByPart(".png")
 
 #The amount of time left pereived by the bomb
 def GetPerceivedBombTime() :
-    return Module.GetModules("MODULE_BOMB")[0].CurrentTimePenalty + CurrentTime
+    
+    return Module.GetModules("MODULE_BOMB")[0].CurrentTimePenalty + GP.CurrentTime
+
+def GetShaderIndex() :
+
+    #Return index of the currently selected shader
+    
+    return (SelectedShader)
+
+def GetNumberOfShaders():
+
+    return NumberOfShaders
 
 
+#TODO: Remove this
 def GetBombTime() :
     
     ThisBombModule = Module.GetModules("MODULE_BOMB")
@@ -123,48 +123,42 @@ def GetBombTime() :
     return ThisBombModule[0].BombTime
 
 def UpdateLogic(MouseCoordsIn, MousePressedIn, ButtonPressesIn, DistanceMesurementIn, TiltedIn, KeyPadInputsIn, GameWindowWidthHeightIn, ScaleFactor, CurrentTimeIn):
+ 
     
-    global MouseCoords
-    global MousePressed 
-    global ButtonPresses
-    global DistanceGameWindowWidthHeightInMesurement
-    global Tilted 
-    global KeyPadInputs 
-    global GameWindowWidthHeight
-    global CurrentTime
-    global GameState
-    global BombTime
 
-    CurrentTime = CurrentTimeIn
+    GP.CurrentTime = CurrentTimeIn
 
-    GameWindowWidthHeight = GameWindowWidthHeightIn.copy()
+    GP.GameWindowWidthHeight = GameWindowWidthHeightIn.copy()
 
     #The mouse coordinates converted to normalized screen coordinates
-    MouseCoords = MouseCoordsIn.copy()
+    
+    GP.MouseCoords = MouseCoordsIn.copy()
   
-    MouseCoords[0] = (MouseCoords[0]/ScaleFactor/GameWindowWidthHeight[0] - 0.5)*2
-    MouseCoords[1] = (MouseCoords[1]/ScaleFactor/GameWindowWidthHeight[1] - 0.5)*2
+    GP.MouseCoords[0] = (GP.MouseCoords[0]/ScaleFactor/GP.GameWindowWidthHeight[0] - 0.5)*2
+    GP.MouseCoords[1] = (GP.MouseCoords[1]/ScaleFactor/GP.GameWindowWidthHeight[1] - 0.5)*2
 
     #Registers if the mouse has been pressed
-    MousePressed = MousePressedIn
+    GP.MousePressed = MousePressedIn
 
     
     #Registers any of the button presses (RGB buttons)
-    ButtonPresses = ButtonPressesIn.copy()
+    GP.ButtonPresses = ButtonPressesIn.copy()
 
     #Registers the distance mesured by the sensor
-    DistanceMesurement = DistanceMesurementIn                                    
+    GP.DistanceMesurement = DistanceMesurementIn                                    
 
     #Registers if the board is tilted or not
-    Tilted = TiltedIn
+    GP.Tilted = TiltedIn
 
     #Registers if the keypad key presses
-    KeyPadInputs = KeyPadInputsIn.copy()
+    GP.KeyPadInputs = KeyPadInputsIn.copy()
+
+    #SetBackroundShader(int(GP.CurrentTime/1000.0)%4)
    
     #Loop through all of the modules
     for ThisModule in Module.CurrentModulesInGame :
 
-        MaxPoint = GetMaxPoint(ThisModule.X, ThisModule.Y, ThisModule.Width, ThisModule.Height, GameWindowWidthHeightIn[0], GameWindowWidthHeightIn[1])
+        MaxPoint = GetMaxPoint(ThisModule.X, ThisModule.Y, ThisModule.Width, ThisModule.Height, GP.GameWindowWidthHeight[0], GP.GameWindowWidthHeight[1])
 
         #The mouse position relative to the module
         MouseRelativeToModule = [1000,1000]
@@ -172,7 +166,7 @@ def UpdateLogic(MouseCoordsIn, MousePressedIn, ButtonPressesIn, DistanceMesureme
         #Array is modified, so copy
         MouseRelativeToModule = MouseCoordsIn.copy()
 
-        ModuleScreenPosition = GetScreenCoordinates(ThisModule.X, ThisModule.Y, GameWindowWidthHeight[0], GameWindowWidthHeight[1])
+        ModuleScreenPosition = GetScreenCoordinates(ThisModule.X, ThisModule.Y, GP.GameWindowWidthHeight[0], GP.GameWindowWidthHeight[1])
 
         #Make the mouse relative to this module. To do that, the mouse position should be relative to the module position and then normalize
         MouseRelativeToModule[0] = ((MouseCoordsIn[0]/ScaleFactor - ModuleScreenPosition[0])/ThisModule.Width - 0.5)*2
@@ -183,10 +177,10 @@ def UpdateLogic(MouseCoordsIn, MousePressedIn, ButtonPressesIn, DistanceMesureme
 
     #Check for a game over. Losing means that the current time is equal to the bomb time, winning means that the bomb is solved
     #if BombTime <= CurrentTime :
-    if Module.GetModules("MODULE_BOMB")[0].CurrentTimePenalty + CurrentTime >= GetBombTime() :
-        GameState = 2
+    if Module.GetModules("MODULE_BOMB")[0].CurrentTimePenalty + GP.CurrentTime >= GetBombTime() :
+        GP.GameState = 2
     elif Module.GetModules("MODULE_BOMB")[0].solved :
-        GameState = 1
+        GP.GameState = 1
 
-    return GameState
+    return GP.GameState
 
